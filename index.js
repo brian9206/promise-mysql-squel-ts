c32037a07b18e7afcd6012a7ace094e3780764e5
Brian <brian9206@hotmail.com>
2017-03-22 15:46:22 +0800

    Add compiled .js

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.__esModule = true;
+var promiseMysql = require("promise-mysql");
+var squel = require("squel");
+var MySql;
+(function (MySql) {
+    // MySql wrapper
+    function createConnection(config) {
+        return promiseMysql.createConnection(config);
+    }
+    MySql.createConnection = createConnection;
+    function createPool(config) {
+        return promiseMysql.createPool(config);
+    }
+    MySql.createPool = createPool;
+    // Squel wrapper
+    MySql.QueryBuilder = squel.useFlavour('mysql');
+    function executeQueryBuilder(conn, params) {
+        return conn.query(params.text, params.values);
+    }
+    MySql.executeQueryBuilder = executeQueryBuilder;
+})(MySql = exports.MySql || (exports.MySql = {}));
