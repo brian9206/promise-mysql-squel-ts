3b14e42c483f2ac548c0128ea2dc3a93b5e5df01
Brian <brian9206@hotmail.com>
2017-03-23 23:50:24 +0800

    Strong typed

--- index.ts
+++ index.ts
@@ -1,20 +1,21 @@
+import * as mysql from 'mysql';
 import * as promiseMysql from 'promise-mysql';
 import * as squel from 'squel';
 
 export namespace MySql {
     // MySql wrapper
-    export function createConnection(config: any): any {
+    export function createConnection(config: mysql.IConnectionConfig): IPromisifiedConnection {
         return promiseMysql.createConnection(config);
     }
 
-    export function createPool(config: any): any {
+    export function createPool(config: mysql.IPoolConfig): IPromisifiedPool {
         return promiseMysql.createPool(config);
     }
     
     // Squel wrapper
-    export let QueryBuilder = squel.useFlavour('mysql');
+    export let QueryBuilder: QueryBuilder = squel.useFlavour('mysql');
 
-    export function executeQueryBuilder(conn: any, params: any) {
+    export function executeQueryBuilder(conn: IPromisifiedConnection, params: any): Promise<mysql.IQuery> {
         return conn.query(params.text, params.values);
     }
 }
